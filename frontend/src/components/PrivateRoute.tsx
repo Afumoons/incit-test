73d6587c3edbf9db6adad3310c70ff8e542dfbef
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-07-29 10:02:49 +0700

    Fix bug not working Cookies.set("token") on email login

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -10,12 +10,10 @@ const PrivateRoute = ({ component: Component }: any) => {
   const getProtectedData = async () => {
     try {
       const response = await protectedData();
-      Cookies.set("token", response.data.token);
-      console.log("token", response.data.token, Cookies.get("token"));
-
       Cookies.set("email", response.data.user.email);
+      Cookies.set("token", response.data.token);
 
-      setIsAuthenticated(!!response.data.token);
+      setIsAuthenticated(!!Cookies.get("token"));
     } catch (error) {
       console.error("Error fetching protected data:", error);
       throw error;
